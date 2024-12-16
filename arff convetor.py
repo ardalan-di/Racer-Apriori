d03b447fd43f34b68fae2f7d0ec577cdf9ed178d
ardalan-di <ardalandelfardi@gmail.com>
2024-12-16 19:17:03 +0330

    .

--- arff convetor.py
+++ arff convetor.py
@@ -12,9 +12,7 @@ filePath = "C:\\Users\Hkr\\Desktop\\bachelor project\\racerCode\\Racer-Apriori\\
 data, meta = arff.loadarff(filePath)
 df = pd.DataFrame(data)
 
-# Ensure that categorical data is properly encoded
-# Convert 'class' column (target variable) to strings (it might be byte literals in ARFF)
-df['Class'] = df['Class'].str.decode('utf-8')
+df = df.applymap(lambda x: x.decode('utf-8') if isinstance(x, bytes) else x)
 
 # Separate features (X) and target (y)
 X = df.drop(columns=['Class'])
@@ -28,16 +26,25 @@ if not os.path.exists(output_folder):
 # Initialize StratifiedKFold to ensure balanced splits
 kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
 
+
 # Iterate through each fold
 fold = 1
-for train_index, test_index in kf.split(X, y):
-    # Split the data into train and test sets
-    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+for train_index, test_index in kf.split(X,y):
+    # Split the data into train and test sets for the fold
+    X_fold = X.iloc[test_index]
+    y_fold = y.iloc[test_index]
+
+    # Now, for each fold, we split the fold into 90% train and 10% test within that fold
+    fold_train_size = int(0.9 * len(X_fold))
+    X_fold_train = X_fold.iloc[:fold_train_size]
+    y_fold_train = y_fold.iloc[:fold_train_size]
+    
+    X_fold_test = X_fold.iloc[fold_train_size:]
+    y_fold_test = y_fold.iloc[fold_train_size:]
     
     # Combine features and target for train and test datasets
-    train_data = pd.concat([X_train, y_train], axis=1)
-    test_data = pd.concat([X_test, y_test], axis=1)
+    train_data = pd.concat([X_fold_train, y_fold_train], axis=1)
+    test_data = pd.concat([X_fold_test, y_fold_test], axis=1)
     
     # Save the train and test files in ARFF format
     # Save train data
@@ -46,7 +53,7 @@ for train_index, test_index in kf.split(X, y):
         # Write ARFF header
         f.write('@relation car\n\n')
         for col in X.columns:
-            f.write(f'@attribute {col} {{' + ', '.join(X[col].unique().astype(str)) + '}}\n')
+            f.write(f'@attribute {col} {{' + ', '.join(X[col].unique().astype(str)) + '}\n')
         f.write('@attribute class {unacc, acc, good, vgood}\n\n')
         f.write('@data\n')
         
@@ -59,7 +66,7 @@ for train_index, test_index in kf.split(X, y):
         # Write ARFF header
         f.write('@relation car\n\n')
         for col in X.columns:
-            f.write(f'@attribute {col} {{' + ', '.join(X[col].unique().astype(str)) + '}}\n')
+            f.write(f'@attribute {col} {{' + ', '.join(X[col].unique().astype(str)) + '}\n')
         f.write('@attribute class {unacc, acc, good, vgood}\n\n')
         f.write('@data\n')
         
